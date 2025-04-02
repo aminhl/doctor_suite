9f368e47fd00bf9de5868c0ffef8367cade70234
aminhl <amin.hlel@esprit.tn>
2025-04-02 20:52:16 +0200

    init commit

---
+++ src/main/java/org/nexthope/doctorsuite/DoctorSuiteApplication.java
@@ -0,0 +1,13 @@
+package org.nexthope.doctorsuite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorSuiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoctorSuiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nexthope/doctorsuite/DoctorSuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nexthope.doctorsuite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorSuiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
