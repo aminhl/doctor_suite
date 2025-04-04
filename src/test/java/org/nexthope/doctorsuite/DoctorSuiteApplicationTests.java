6733612c097669199d8719362dc23a3770dba04e
aminhl <amin.hlel@esprit.tn>
2025-04-04 23:21:10 +0200

    tests: init unit test config

--- src/test/java/org/nexthope/doctorsuite/DoctorSuiteApplicationTests.java
+++ src/test/java/org/nexthope/doctorsuite/DoctorSuiteApplicationTests.java
@@ -1,13 +1,20 @@
 package org.nexthope.doctorsuite;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class DoctorSuiteApplicationTests {
 
+    @Mock
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        Assertions.assertThat(applicationContext).isNotNull();
     }
 
 }
